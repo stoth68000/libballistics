d696745a5e7bf8ba349d465ba225a00ef2d900bd
Steven Toth <stoth@kernellabs.com>
2017-05-29 13:40:14 -0400

    Checkin

--- tools/example.c
+++ tools/example.c
@@ -23,21 +23,21 @@
 int main(int argc, char *argv[]) {
   int s, k = 0;
   ballistics_ctx_t context;   /* Our ballistics context */
-  double bc=0.495;            /* Ballistic coefficient of the projectile */
-  double v=2600;              /* Muzzle velocity (fps) */
-  double w=175;               /* Bullet weight (gr) */
-  double sh=1.5;              /* Sight height (in) */
+  double bc=0.475;            /* Ballistic coefficient of the projectile */
+  double v=2742;              /* Muzzle velocity (fps) */
+  double w=168;               /* Bullet weight (gr) */
+  double sh=2.5;              /* Sight height (in) */
   double angle=0;             /* LOS Angle (degrees) */
-  double zero=300;            /* Zero range (yards) */
-  double windspeed=10;        /* Wind speed (MPH) */
+  double zero=100;            /* Zero range (yards) */
+  double windspeed=25;        /* Wind speed (MPH) */
   double windangle=90;        /* Wind angle (degrees) */
   double zeroangle=0;         /* Zero angle of the bore */
 
   /* Custom atmosphere */
   double altitude = 0.0,
-         pressure = 29.92,
-         temperature = 59.0,
-         humidity = .78; 
+         pressure = 29.97,
+         temperature = 56.4,
+         humidity = .87; 
 
   /* If you wish to adjust for atmospheric conditions, correct the BC */
 
@@ -67,7 +67,7 @@ int main(int argc, char *argv[]) {
   k = libballistics_computeTrajectory(context, G1, v, sh, angle, 
       zeroangle, windspeed, windangle, 1200);
 
-  for (s=25; s<=1200; s+=25) {
+  for (s = zero; s < (zero + 1200); s += 100) {
       printf(" %4.0f  %+6.1f %+6.1f  %+6.1f  %+6.1f      %04.0f     %04.0f"
              "    %01.2f\n",
       libballistics_getRange(context, s),
